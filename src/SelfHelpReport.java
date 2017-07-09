2c871f684c61b95484021db7ec013225e3bd847f
priyaprasad27 <pprasad@villanova.edu>
2017-07-09 00:33:47 -0400

    resolving conflict

--- src/SelfHelpReport.java
+++ src/SelfHelpReport.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 public class SelfHelpReport implements ReportGenerator{
 
 	private String selfHelpName;
@@ -22,28 +20,3 @@ public class SelfHelpReport implements ReportGenerator{
 	}
 
 }
-=======
-
-public class SelfHelpReport implements ReportGenerator{
-
-	private String selfHelpName;
-	private int noOfTimesUsed;
-	private String effectivity;
-	
-	
-	@Override
-	public String generateReport() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("Self Help Name : ");
-		builder.append(selfHelpName + "\n");
-		builder.append("Number of Times Used : ");
-		builder.append(noOfTimesUsed + "\n");
-		builder.append("Effectivity : ");
-		builder.append(effectivity + "\n");
-		
-				
-		return builder.toString();
-	}
-
-}
->>>>>>> dev
