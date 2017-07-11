d4007edce1f2e9c55548f09328d39429e61b18ff
priyaprasad27 <pprasad@villanova.edu>
2017-07-10 22:45:50 -0400

    self help

---
+++ src/SelfHelp.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+import java.util.List;
+
+public class SelfHelp {
+
+	private String selfHelpName;
+	private static int timesUsed;
+
+	public SelfHelp(String name){
+		this.selfHelpName = name;
+					
+	}
+	
+	
+	
+}
